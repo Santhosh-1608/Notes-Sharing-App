d0775366e5d3dfa7c840e626614c0e8937229f2a
Santhosh-1608 <santhoshv1608@gmail.com>
2025-02-24 20:07:33 +0530

    new commit

--- NotesSharingProject/settings.py
+++ NotesSharingProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n3w0&qhhe(wnk_a7fb*n$#8^u)*3y*u&o2k#m(2_!n-1ogq^zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.4","localhost","127.0.0.1",'*']
 
 
 # Application definition
