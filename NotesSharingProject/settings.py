2739210eed80559acf68febc8191b90d59096d7a
Santhosh V <143200557+Santhosh-1608@users.noreply.github.com>
2025-03-07 10:41:16 +0530

    Update settings.py

--- NotesSharingProject/settings.py
+++ NotesSharingProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n3w0&qhhe(wnk_a7fb*n$#8^u)*3y*u&o2k#m(2_!n-1ogq^zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.4","localhost","127.0.0.1",'*']
+ALLOWED_HOSTS = ["192.168.0.4","localhost","127.0.0.1",'*',"notes-sharing-app-5pso.onrender.com"]
 
 
 # Application definition
