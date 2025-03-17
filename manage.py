bc62fe82121bdeb4dd969a27a791b78513775824
Santhosh V <143200557+Santhosh-1608@users.noreply.github.com>
2025-03-17 15:02:08 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,22 +3,24 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NotesSharingProject.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management.commands.runserver import Command as runserver
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    # Override default port for `runserver` command
+    runserver.default_port = os.environ.get('PORT', '8000')
+    runserver.default_addr = '0.0.0.0'
+
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
-
